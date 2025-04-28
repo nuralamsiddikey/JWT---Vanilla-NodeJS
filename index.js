ced3ae2ce8b79e391ed2b36f5f8428d2466c1816
nuralamsiddikey <nurcse253@gmail.com>
2025-04-28 22:44:42 +0600

    added

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import profileRoutes from "./routes/profileRoutes.js";
 
 dotenv.config();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000
 
 
 const server = http.createServer(async (req, res) => {
